05b7b2666fa8bed27cd9a1eaf417e70fbe6d47e4
Varad Sharma <varad.code19@gmail.com>
2026-01-13 17:40:11 +0530

    init

---
+++ patch/include.c
@@ -0,0 +1,25 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+
+/* Other patches */
+#include "autostart.c"
+#include "recttoclient.c"
+#include "fullscreen.c"
+#include "pertag.c"
+#include "placemouse.c"
+#include "renamed_scratchpads.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
