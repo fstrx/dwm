7fee6da318938d80b7e4a536f3365c5bb6c0e392
Varad Sharma <varad.code19@gmail.com>
2026-03-09 00:28:40 +0530

    10tags

--- config.h
+++ config.h
@@ -150,10 +150,10 @@ static const char *scratchpadcmd[] = {"s", "st", "-n", "spterm", NULL};
  * patches.
  */
 static char *tagicons[][NUMTAGS] = {
-    [DEFAULT_TAGS] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"},
-    [ALTERNATIVE_TAGS] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"},
+    [DEFAULT_TAGS] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
+    [ALTERNATIVE_TAGS] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
     [ALT_TAGS_DECORATION] = {"<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>",
-                             "<8>", "<9>"},
+                             "<8>", "<9>", "<10>"},
 };
 
 /* There are two options when it comes to per-client rules:
@@ -272,22 +272,22 @@ static const Key keys[] = {
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY | Mod4Mask, XK_u, incrgaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_u, incrgaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_i, incrigaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_i, incrigaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_o, incrogaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_o, incrogaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_6, incrihgaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_6, incrihgaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_7, incrivgaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_7, incrivgaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_8, incrohgaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_8, incrohgaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_9, incrovgaps, {.i = +1}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_9, incrovgaps, {.i = -1}},
-    {MODKEY | Mod4Mask, XK_0, togglegaps, {0}},
-    {MODKEY | Mod4Mask | ShiftMask, XK_0, defaultgaps, {0}},
+    {MODKEY | Mod1Mask, XK_u, incrgaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_u, incrgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_i, incrigaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_i, incrigaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_o, incrogaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_o, incrogaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_6, incrihgaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_6, incrihgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_7, incrivgaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_7, incrivgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_8, incrohgaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_8, incrohgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_9, incrovgaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_9, incrovgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_0, togglegaps, {0}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_0, defaultgaps, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     {MODKEY | ShiftMask, XK_q, quit, {0}},
@@ -300,7 +300,7 @@ static const Key keys[] = {
     {MODKEY | ControlMask, XK_grave, setscratch, {.v = scratchpadcmd}},
     {MODKEY | ShiftMask, XK_grave, removescratch, {.v = scratchpadcmd}},
     {MODKEY | ShiftMask, XK_f, fullscreen, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
+    {MODKEY, XK_F1, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
     {MODKEY, XK_comma, focusmon, {.i = -1}},
     {MODKEY, XK_period, focusmon, {.i = +1}},
@@ -311,7 +311,7 @@ static const Key keys[] = {
     {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8)};
+            TAGKEYS(XK_9, 8) TAGKEYS(XK_0, 9)};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
